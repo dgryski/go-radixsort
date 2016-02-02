8410589e3f15038b3854ad214c0581ee8b65165b
Damian Gryski <dgryski@gmail.com>
2016-02-02 08:18:13 +0100

    add type-specialized Ints() and Uints64()

--- sort.go
+++ sort.go
@@ -46,3 +46,81 @@ func Sort(E []Elem) {
 		src, dst = dst, src
 	}
 }
+
+// Ints sorts ints in O(n) time with O(n) extra space.
+func Ints(E []int) {
+
+	// Algorithm from CLRS, chapter 8
+
+	const m = 256
+	n := len(E)
+
+	counts := make([]int, m)
+
+	src := E
+	dst := make([]int, n)
+
+	for i := uint(0); i < 8; i++ {
+		shift := i * 8
+
+		for i := range counts {
+			counts[i] = 0
+		}
+
+		for i := 0; i < n; i++ {
+			j := byte(src[i] >> shift)
+			counts[j]++
+		}
+
+		for j := 1; j < m; j++ {
+			counts[j] += counts[j-1]
+		}
+
+		for i := n - 1; i >= 0; i-- {
+			j := byte(src[i] >> shift)
+			dst[counts[j]-1] = src[i]
+			counts[j]--
+		}
+
+		src, dst = dst, src
+	}
+}
+
+// Uint64s sorts uint64s in O(n) time with O(n) extra space.
+func Uint64s(E []uint64) {
+
+	// Algorithm from CLRS, chapter 8
+
+	const m = 256
+	n := len(E)
+
+	counts := make([]uint64, m)
+
+	src := E
+	dst := make([]uint64, n)
+
+	for i := uint(0); i < 8; i++ {
+		shift := i * 8
+
+		for i := range counts {
+			counts[i] = 0
+		}
+
+		for i := 0; i < n; i++ {
+			j := byte(src[i] >> shift)
+			counts[j]++
+		}
+
+		for j := 1; j < m; j++ {
+			counts[j] += counts[j-1]
+		}
+
+		for i := n - 1; i >= 0; i-- {
+			j := byte(src[i] >> shift)
+			dst[counts[j]-1] = src[i]
+			counts[j]--
+		}
+
+		src, dst = dst, src
+	}
+}
